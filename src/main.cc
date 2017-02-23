eb9e584165a08e88afb593ac5425fe0729dbd1be
Carushi <trumpet-lambda@hotmail.co.jp>
2017-02-23 15:15:39 +0900

    edit help messages

--- src/main.cc
+++ src/main.cc
@@ -17,67 +17,80 @@ void PrintHelpOption()
          << "This algorithm has been developed in github.com/carushi/ParasoR.\n"
          << "We would like to express our gratitude to Vienna RNA package and CentroidFold for developing ParasoR.\n\n"
          << "You can achieve calculation of base pairing probability or accessibility for sequences of any nucleotides long by ParasoR.\n\n";
-    cout << "-------Option List------\n"
-         << "--pre\tnot-parallel computing\n"
-         << "--stem\tcalculate stem probability\n"
-         << "--bpp (or --bpp=minp)\tcalculate base pairing probability (output base pairing probability >= minp)\n"
-         << "--entropy\tcalculate entropy (sum of -plog2p)\n";
-    cout << "--acc\t(-a)\tuse accessibility \n\t(default: stem probability)\n"
-         << "--prof\t(-p -a)\tuse profile vectors\n"
-         << "--motif\t(-p)\tprint a motif sequence\n"
-         << "--struct (or --struct=gamma)\toutput a centroid structure with gamma centroid estimator (whose base pairs having bpp (>= 1/(gamma+1) and length is less than 600 nt) \n"
-         << "--image\t\toutput images of gamma centroid estimated structure (only with struct option)\n"
-         << "-c\tprint correlation coefficient of distance (eucledian, eSDC)\n"
-         << "-r\tuse complementary sequence\n\n"
-         << "-m [S or D or I or nan]\tcalculate mutated probability around single point Substitution or Deletion or Insertion from start to end\n"
-         << "--mout [outfile or nan]\toutput a result of mutatiing simulation to stdout or outfile\n\n"
-         << "-s [num]\tstart point of base pairing probability (1~(sequence length-1)) \n"
-         << "-e [num]\tend point of base pairing probability\n\t\tif you assign only a start position for one sequence, it'll be the sequence length.\n"
-         << "-i [num]\tthe number of chunk id  0~(#chunk-1)\n\t\t(if you assign #chunk, it'll make connected file in the same way as connect option.)\n"
-         << "-k [num]\tthe max number of chunk\n"
-         << "-f [sequence]\tdirect input of sequence\n\n"
-         << "-T [temperature]\tchange temperature\n"
-         //<< "-d\tfor debug\n"
-         << "-t\tkeep temporary douter files after connecting (only valid with connect option)\n\n"
-         << "--constraint [num]\tset the maximal span to num\n"
-         << "--input [filename]\tdeal filename as sequence file input\n"
-         // << "--outerinput [filename]\tdeal filename as douter file input\n"
-         << "--name [name]\tprefix of db filename\n"
-         << "--cd\t!!!warning!!! use another directory via a relative path. (Please confirm existence of outer and prob directory.)\n"
-         << "--divide\tcalculate divided douters and make temporal douter files (default)\n"
-         << "--connect\tconnect douters and make douter_inside & outside file\n"
-         << "--stemdb\toutput stem probability database\n"
-         << "--energy [.par file]\talso possible to use \"Turner2004\", \"Andronescu\", and \"Turner1999\" as an abbreviation\n"
-         << "--boundary\tcalculate a probability having no base pair with its right side at each position (only valid without pre option)\n"
-         << "--text\ttext storage mode with low accuracy\n"
-         << "--stdout\ttext output mode (only valid with stemdb option)\n"
-         << "--save_memory\tsave memory mode in Divide and Connect procedure\n"
-         << "--init_file\tremove temp files for connect procedure (only valid with --connect and --save_memory option)\n"
-         // << "--remote [input]\tcalculate the stability of remote duplexes (not completed)\n"
-         // << "--transcribed\tsimulate the arrangement of structure during transcription (not completed)\n"
-         << "--help\tprint these sentences\n\n\n\n";
+    cout << "-------Option List------\n\n"
+         << "--pre\tnot-parallel computing (Rfold and CapR)\n"
+         << "(without --pre)\tparallel computing (ParasoR) \n"
+         << "--help\tprint detailed help\n\n";
     cout << "-------Sample List------\n"
          << "Rfold and CapR:\n"
          << "\t\t./ParasoR --pre --bpp // use a default p53 sequence;\n"
          << "\t\t./ParasoR --pre --stem --input [input fasta]\n"
          << "\t\t./ParasoR --pre --prof --struct -f [sequence]\n"
-         << "Parallel computing:\n"
+         << "\nParallel computing:\n"
          << "\t\t./ParasoR --name seq1 -f [sequence] -i 0 -k 2 // Divide procedure #0\n"
          << "\t\t./ParasoR --name seq1 -f [sequence] -i 1 -k 2 // Divide procedure #1\n"
          << "\t\t./ParasoR --name seq1 -f [sequence] -i 2 -k 2 // Connect procedure;\n"
-         << "\t\t./ParasoR --name seq1 -f [sequence] --stem // Calculating stem probability;\n"
-         << "\t\t./ParasoR --name seq1 -f [sequence] -i 0 -k 2 --stemdb // Making stemdb;\n"
-         << "Sample2:\n"
+         << "\t\t./ParasoR --name seq1 -f [sequence] --stem // Calculate stem probability for an entire sequence;\n"
+         << "\t\t./ParasoR --name seq1 -f [sequence] -i 0 -k 2 --stemdb // Construct stemdb for the first fragment;\n"
+         << "\t\t./ParasoR --name seq1 -f [sequence] -i 0 -k 2 --stemdb --stdout // Print stemdb;\n"
+         << "\nSample2:\n"
          << "\t\t./ParasoR --name seq1 --energy A --input [input fasta] --bpp -s 10 -e 20\n"
-         << "\t\t./ParasoR --name seq1 --energy T --input [input fasta] -i 0 -k 1 --stemdb -a\n"
-         << "Sample3: (not parallel)\n"
+         << "\t\t./ParasoR --name seq1 --energy ../energy_param/rna_turner2004_new.par --input [input fasta] -i 0 -k 1 --stemdb -a\n"
+         << "\nSample3: (not parallel)\n"
          << "\t\t./ParasoR --name test -f [sequence] --pre --bpp\t\t// base pairing probability;\n"
          << "\t\t./ParasoR --name test -f [sequence] --pre --stem\t\t// stem probability;\n"
-         << "\t\t./ParasoR --name test -f [sequence] --pre -a\t\t// accessibility;\n"
+         << "\t\t./ParasoR --name test -f [sequence] --pre --acc\t\t// accessibility;\n"
          << "\t\t./ParasoR --name test -f [sequence] --pre --prof\t\t// profile;\n"
          << "\t\t./ParasoR --name test -f [sequence] --pre --motif\t\t// profile string;\n"
          << "\t\t./ParasoR --name test -f [sequence] --pre --struct --image\t// centroid struct image;\n"
          << endl;
+}
+
+void PrintDetailedHelpOption()
+{
+    cout << "ParasoR (Parallel solution of RNA secondary structure prediction)\n\n"
+         << "Detailed options:\n\n"
+         << "-------Running mode------\n\n"
+         << "--pre\n\tnot-parallel computing (Rfold and CapR)\n"
+         << "(without --pre)\n\tparallel computing (ParasoR) \n"
+         << "--help\n\tprint detailed help\n"
+         << "--divide\n\tcalculate divided douters and make temporal douter files (default)\n"
+         << "\t-i [num]\n\t\tthe number of chunk id  0~(#chunk-1)\n\t\t(if you assign #chunk, it'll make connected file in the same way as connect option.)\n"
+         << "\t-k [num]\n\t\tthe max number of chunk\n"
+         << "--connect\n\tconnect douters and make douter_inside & outside file\n"
+         << "--stemdb\n\toutput stem probability database\n"
+         << "-m [S or D or I or nan]\n\tcalculate mutated probability around single point Substitution or Deletion or Insertion from start to end\n"
+         << "\t--mout [outfile or nan]\n\t\toutput a result of mutatiing simulation to stdout or outfile\n"
+         << "--save_memory\n\tsave memory mode in Divide and Connect procedure\n"
+         << "--text\n\ttext storage mode with low accuracy\n\n";
+    cout << "-----Subject features-----\n\n"
+         << "--stem\n\tcalculate stem probability\n"
+         << "--bpp (or --bpp=minp)\n\tcalculate base pairing probability (output base pairing probability >= minp)\n"
+         << "--entropy\n\tcalculate entropy (sum of -plog2p)\n";
+    cout << "--acc\t(-a)\n\tuse accessibility\n\t(default: stem probability)\n"
+         << "--prof\t(-p -a)\n\tuse profile vectors\n"
+         << "--motif\t(-p)\n\tprint a motif sequence\n"
+         << "--struct (or --struct=gamma)\n\toutput a centroid structure with gamma centroid estimator (whose base pairs having bpp (>= 1/(gamma+1) and length is less than 600 nt) \n"
+         << "--image\n\toutput images of gamma centroid estimated structure (only with struct option)\n"
+         << "--boundary\n\tcalculate a probability having no base pair with its right side at each position (only valid without pre option)\n"
+         << "-c\n\tprint correlation coefficient of distance for mutating simulation (eucledian, eSDC)\n\n";
+    cout << "----Parameter setting----\n\n"
+         << "--constraint [num]\n\tset the maximal span to num\n"
+         << "-s [num]\n\tstart point of base pairing probability (1~(sequence length-1)) \n"
+         << "-e [num]\n\tend point of base pairing probability\n\t\tif you assign only a start position for one sequence, it'll be the sequence length.\n"
+         << "-T [temperature]\n\tchange temperature (not completed!)\n\n"
+         << "--energy [.par file]\n\talso possible to use \"Turner2004(old)\", \"Andronescu\", and \"Turner1999\" as an abbreviation\n\n";
+    cout << "------I/O setting------\n\n"
+         << "-r\n\tuse complementary sequence\n"
+         << "-f [sequence]\n\tdirect input of sequence\n"
+         << "--input [filename]\n\tdeal filename as sequence file input\n"
+         << "--name [name]\n\tprefix of db filename\n"
+         << "-t\n\tkeep temporary douter files after connecting (only valid with connect option)\n\n"
+         << "--cd\n\t!!!warning!!! use another directory via a relative path. (Please confirm existence of outer and prob directory.)\n"
+         << "--stdout\n\ttext output mode (only valid with stemdb option)\n"
+         << "--init_file\n\tremove temp files for connect procedure (only valid with --connect and --save_memory option)\n";
+         // << "--remote [input]\tcalculate the stability of remote duplexes (not completed)\n"
+         // << "--transcribed\tsimulate the arrangement of structure during transcription (not completed)\n"
 }
 
 struct option* option()
@@ -194,7 +207,7 @@ bool SetArg(int option_index, const char* optarg, Rfold::Arg& arg)
                 arg.init_calc = Rfold::Arg::Calc::Bpp;
             break;
         default:
-            PrintHelpOption();
+            PrintDetailedHelpOption();
             return false;
     }
     return true;
